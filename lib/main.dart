b1e3059532b26e9371ade84c5e7868fe86f5a975
坂本矩崇 <nrtk0320@gmail.com>
2023-01-18 20:49:30 +0900

    完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,6 +25,47 @@ class JankenPage extends StatefulWidget {
 }
 
 class _JankenPageState extends State<JankenPage> {
+  String computerHand = '✊';
+  String myHand = '✊';
+  String result = '引き分け';
+
+  void selectHand(String selectedHand) {
+    myHand = selectedHand;
+    generateComputerHand();
+    judge();
+    setState(() {});
+  }
+
+  void generateComputerHand() {
+    final randomNumber = Random().nextInt(3);
+    computerHand = randomNumberToHand(randomNumber);
+  }
+
+  String randomNumberToHand(int randomNumber) {
+    switch (randomNumber) {
+      case 0:
+        return '✊';
+      case 1:
+        return '✌️';
+      case 2:
+        return '🖐';
+      default:
+        return '✊';
+    }
+  }
+
+  void judge() {
+    if (computerHand == myHand) {
+      result = '引き分け';
+    } else if (myHand == '✊' && computerHand == '✌️' ||
+        myHand == '✌️' && computerHand == '🖐' ||
+        myHand == '🖐' && computerHand == '✊') {
+      result = '勝ち';
+    } else {
+      result = '負け';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,26 +73,50 @@ class _JankenPageState extends State<JankenPage> {
         title: Text('じゃんけん'),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                print('✊');
-              },
-              child: Text('✊'),
+            Text(
+              result,
+              style: TextStyle(fontSize: 32),
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('✌️');
-              },
-              child: Text('✌️'),
+            SizedBox(height: 64),
+            Text(
+              'computer',
+              style: TextStyle(fontWeight: FontWeight.bold), 
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('✋');
-              },
-              child: Text('✋'),
+            Text(
+              computerHand,
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(height: 64),
+            Text(
+              myHand,
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✊');
+                  },
+                  child: Text('✊'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('✌️');
+                  },
+                  child: Text('✌️'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    selectHand('🖐');
+                  },
+                  child: Text('🖐'),
+                ),
+              ],
             ),
           ],
         ),
